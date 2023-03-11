1626d114d5b49abc7858181949acb5904e02d301
Nacho Álvarez <pgp@nachoalvarez.dev>
2023-03-11 03:40:13 -0300

    fix: back to entrypoint and use init script

--- nachorand-tss/server-command.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-while [ -z "${TOKEN}" ]; do 
-  echo "Waiting for logs..."
-  sleep 1
-  export TOKEN=$(grep -m1 -h -oP 'token=\K\S+' ./logs/*)
-done
---
+++ nachorand-tss/server-entrypoint.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+while [ -z "${TOKEN}" ]; do 
+  echo "Waiting for logs..."
+  sleep 1
+  export TOKEN=$(grep -m1 -h -oP 'token=\K\S+' /teamspeak/logs/*)
+done & /teamspeak/init
