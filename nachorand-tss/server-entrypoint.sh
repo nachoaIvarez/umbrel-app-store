856e2d4b04e923a8ae47e2a88c818b0633a91c4f
Nacho Álvarez <pgp@nachoalvarez.dev>
2023-03-11 03:54:08 -0300

    fix: add tail, pls don't die

--- nachorand-tss/server-entrypoint.sh
+++ nachorand-tss/server-entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
+
 while [ -z "${TOKEN}" ]; do 
   echo "Waiting for logs..."
   sleep 1
   export TOKEN=$(grep -m1 -h -oP 'token=\K\S+' /teamspeak/logs/*)
-done & /teamspeak/init
+done & /teamspeak/init & tail -f /dev/null
